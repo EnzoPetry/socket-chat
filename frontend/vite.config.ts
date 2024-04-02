4c9d43db3062f88465bfe0ee6f83d45af0b317bf
Enzo Petry Ortiz <enzopetryort@gmail.com>
2024-04-02 00:45:07 -0300

    feat: Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173, // Port used by docker container
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
